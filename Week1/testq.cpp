6c2f39e5688c197a531685c3b95eff2ca79e8bb1
farzeensarwar73-byte <farzeensarwar73@gmail.com>
2026-02-22 15:15:25 +0500

    program

---
+++ Week1/testq.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+ 
+
+  cout << "     -----------------------  \n " ;
+  cout << "           o  ^__^            \n " ;
+  cout << "            o (oo)\\_____      \n " ;
+  cout << "              (__)     )\\/\\  \n " ;
+  cout << "                  ||----w |   \n " ;
+  cout << "                  ||     ||   \n " ;
+}
